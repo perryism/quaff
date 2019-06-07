75f2a71a5664f7c5b9e18413a91b763691966068
Perry <perryism@gmail.com>
2019-06-07 00:08:44 -0700

    Add annotations_items

--- quaff/__init__.py
+++ quaff/__init__.py
@@ -1,1 +1,8 @@
+__all__ = ["annotations_items", "quaff"]
+
+def annotations_items(func):
+    for var_name, tipe in func.__annotations__.items():
+       if var_name == "return" : continue
+       yield var_name, tipe
+
 from .quaff import quaff
