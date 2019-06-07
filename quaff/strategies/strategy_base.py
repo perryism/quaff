e8fe235fb03efabe0dfcfac3f473283f62947695
Perry <perryism@gmail.com>
2019-06-07 00:09:38 -0700

    Add StrategyBase

---
+++ quaff/strategies/strategy_base.py
@@ -0,0 +1,16 @@
+from .. import annotations_items
+
+class StrategyBase:
+    def before_setup(self, func):
+        raise NotImplementedError
+    def before_return(self, func, wrapped_f):
+        raise NotImplementedError
+    def get_var(self, var_name):
+        raise NotImplementedError
+
+    def get_args(self, func):
+        args = {}
+        for var_name, tipe in annotations_items(func):
+            if var_name == "return" : continue
+            args[var_name] = tipe(self.get_var(var_name))
+        return args
