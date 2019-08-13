564de95d0f026b152e25178b009ad8b9b2c1ad7b
Perry <perryism@gmail.com>
2019-08-13 13:23:41 -0700

    Include subfolder

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='quaff',
-      version='0.3',
-      packages=['quaff'],
+      version='0.3.1',
+      packages=['quaff', 'quaff/strategies'],
       install_requires=['flask'],
       )
