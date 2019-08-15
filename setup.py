c2a6477dc6c110e76aa7a713653b7cadd1083fbc
Perry <perryism@gmail.com>
2019-08-15 13:49:06 -0700

    Bump package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='quaff',
-      version='0.4.0',
+      version='0.4.1',
       packages=['quaff', 'quaff/strategies'],
       install_requires=['flask'],
       )
