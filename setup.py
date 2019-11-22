47d2e3a9b063ed41b09e15b685f51d3671e2b7c4
Perry <perryism@gmail.com>
2019-11-22 15:47:37 -0800

    Require Jinja2

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup(name='quaff',
       version='0.4.1',
       packages=['quaff', 'quaff/strategies'],
-      install_requires=['flask'],
+      install_requires=['flask', 'Jinja2'],
       )
